d1cc54eeecc420c500e73ecaf5d545ebfb63851c
huskeypm <huskeypm@gmail.com>
2017-05-25 10:36:12 +0800

    added

--- che580/md/run.bash
+++ che580/md/run.bash
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH=/usr/local/NAMD_2.11_Linux-x86_64-multicore:$LD_LIBRARY_PATH
 
-cp /net/share/shared/Examples_and_Tutorials/namdExample/production30.* .
-cp /net/share/shared/Examples_and_Tutorials/namdExample/plb_popcwi.p* .   
+#cp /net/share/shared/Examples_and_Tutorials/namdExample/production30.* .
+#cp /net/share/shared/Examples_and_Tutorials/namdExample/plb_popcwi.p* .   
 
 /usr/local/bin/namd2 production_iter30.conf
