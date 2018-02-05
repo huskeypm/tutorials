a5c6de32dcfcd42c8231f317964b900474862d41
huskeypm <huskeypm@gmail.com>
2018-02-05 08:38:10 -0500

    Minor editing

--- che580/gaussian/hollyJob_His.bash
+++ che580/gaussian/hollyJob_His.bash
@@ -1,8 +1,7 @@
 #!/bin/bash 
 #SBATCH -p limited # partition (queue) for CHE580 
 module load gaussian/G09 
-#g09 h_scrf.com
-export FILE=guan
-g09 ${FILE}_opt.com 
-g09 ${FILE}_scrf.com
+g09 h_scrf.com
+g09 histidine_opt.com 
+g09 histidine_freq.com
 
