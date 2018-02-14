0e30044d211ea5ca056920399407d981ef6bce99
huskeypm <huskeypm@gmail.com>
2018-02-13 20:59:11 -0500

    Adjusted readme, prepared bash script for slurm

--- che580/gaussian_tst/holly.bash
+++ che580/gaussian_tst/holly.bash
@@ -1,8 +1,10 @@
+#!/bin/bash
 #SBATCH -p limited # partition (queue) for CHE580 
 module load gaussian/G09
 g09 HCN_HF_631G_Opt.com
 g09 HNC_HF_631G_Opt.com
 g09 TS_HF_631G_Opt.com
 g09 TS_HF_631G_IRC.com
+g09 TS_HF_631G_SCRF.com
 
 
