16dfe99e18ad33cece3b1c73fe0c44ee02addbc8
huskeypm <huskeypm@gmail.com>
2018-02-20 15:12:59 -0500

    added/montecarlo scripts

--- che580/montecarlo/ising.py
+++ che580/montecarlo/ising.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import numpy as np 
+import matplotlib
+matplotlib.use('Agg') 
 import matplotlib.pylab as plt 
 """
 Bare bones ising model 
@@ -12,14 +14,34 @@ from random import randrange,choice,random
 from numpy import zeros
 
 # initialize random lattice 
-def init_ising_lattice(n):
+def initIsingLattice(n):
     rands= np.reshape(np.random.rand(n*n),[n,n]) 
-    print rands 
+    #print rands 
     lattice = np.ones([n,n],dtype=int) 
     lattice[ rands>0.5 ] = -1
-    print lattice 
+    #print lattice 
 
     return lattice
+
+# score entire lattice
+# This is a terrible approach; the scoring can definitely be vectorized
+# but I couldn't think of a way to do this for an arbitrary scoring
+# function using nearest-neighbors that wrap
+def scoreIsingLattice(lattice,n,J,H):
+    latticeEnergies = np.ones([n,n])*1e9
+    for i in range(n):
+      for j in range(n):
+        S0 = lattice[i,j]
+        Sn = sumNeigh(lattice,i,j,n) 
+        energyCurr= energyij(S0,Sn,J,H)  
+        latticeEnergies[i,j] = energyCurr 
+    return latticeEnergies
+
+# sum nearest neighbors
+def sumNeigh(lattice,i,j,n): 
+  Sn = lattice[(i-1)%n,j]+lattice[(i+1)%n,j]+\
+    lattice[i,(j-1)%n]+lattice[i,(j+1)%n]
+  return Sn 
 
 # energy between spin 0 and its neighbors
 # J is a coupling constant
@@ -42,34 +64,25 @@ def ising(
     ):
     nsteps = np.int(nsteps) 
 
+    # Initialize lattice 
     if lattice is None: 
       print "Initializing lattice" 
-      lattice = init_ising_lattice(n)
-
-    print "I donlt like the energy diff approach - may change this" 
-    energy = 0
-    energies = np.zeros(nsteps)
-    def sumNeigh(lattice,i,j,n): 
-        Sn = lattice[(i-1)%n,j]+lattice[(i+1)%n,j]+\
-             lattice[i,(j-1)%n]+lattice[i,(j+1)%n]
-        return Sn 
+      lattice = initIsingLattice(n)
 
 
-    print "WARNING: this needs to be initialized using actual energy ni a smarter way" 
-    latticeEnergies = np.ones([n,n])*1e9
-    for i in range(n):
-      for j in range(n):
-        S0 = lattice[i,j]
-        Sn = sumNeigh(lattice,i,j,n) 
-        energyCurr= energyij(S0,Sn,J,H)  
-        latticeEnergies[i,j] = energyCurr 
+    # Get initial energy for all elements in lattice 
+    latticeEnergies = scoreIsingLattice(lattice,n,J,H)
 
    
     # simulator 
+    energy = 0
+    energies = np.zeros(nsteps)
     for step in range(nsteps):
         # gen random i,j over range 
         i = randrange(n)
         j = randrange(n)
+
+        ## Evaluate energy of point i,j
         # sums up the neighboring spins
         Sn = sumNeigh(lattice,i,j,n) 
         #dE = energydiff(lattice[i,j],Sn,J,H)
@@ -80,21 +93,24 @@ def ising(
         energyTrial = energyij(ST,Sn,J,H) 
         dE = energyTrial - energyCurr 
 
-        # Metropolis criterion for accepting a spin flip  
+        ## Metropolis criterion for accepting a spin flip  
         if dE < 0 or random() < exp(-dE/T):
             lattice[i,j] = -lattice[i,j]
             latticeEnergies[i,j] = energyTrial    
             energy += dE
+
+        ## score system energy 
         energies[step] = energy
     return lattice,latticeEnergies, energies
 
 def saveImg(lattice,title=None,fname="ising.png"):
     # creates a snapshot image of the current state of the lattice
     plt.figure()
-    print "add routine to make aspect = exact" 
-    plt.pcolormesh(lattice,cmap='gray') 
+    ax = plt.subplot(111) 
+    ax.pcolormesh(lattice,cmap='gray') 
     if title is not None:
-      plt.title(title) 
+      ax.set_title(title) 
+    ax.axis('equal')
     plt.gcf().savefig(fname)
     return 
     
@@ -113,14 +129,15 @@ def main():
         elif key == '-h': H = float(val)
         elif key == '-j': J = float(val)
 	elif key == '-t': T = float(val)
-    lattice,energies = ising(n,nsteps,H,J,T)
+    lattice,latticeEnergies,energies = ising(n,nsteps,H,J,T)
+
     saveImg(lattice,title="T=%f"%T)
     plot(energies)
     return
 
 def plot(energies):
     plt.plot(energies)
-    plt.gcf().savefig("energies") 
+    plt.gcf().savefig("energies.png") 
     return
 
 if __name__ == '__main__': main()
