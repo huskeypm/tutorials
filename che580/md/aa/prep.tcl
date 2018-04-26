205eba2a7c66557567a0a3870727d6d78307e8e1
huskeypm <huskeypm@gmail.com>
2018-04-26 10:01:29 -0400

    minor fix

--- che580/md/aa/prep.tcl
+++ che580/md/aa/prep.tcl
@@ -1,10 +1,10 @@
 
 package require psfgen	 
 topology top_all27_prot_lipid.inp	 
-#pdbalias residue HIS HSE	 
-#pdbalias atom ILE CD1 CD	 
-segment U {pdb lys.pdb}	 
-coordpdb lys.pdb U	 
+pdbalias residue HIS HSE	 
+pdbalias atom ILE CD1 CD	 
+segment U {pdb ile.pdb}	 
+coordpdb ile.pdb U	 
 guesscoord	 
 writepdb fin.pdb	 
 writepsf fin.psf 
